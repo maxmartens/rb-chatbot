2e256eea9cd5a6c2b9cac2ad720bed2c287b613f
Max Martens <mmartens@Maxs-MBP.fritz.box>
2022-01-26 20:23:03 +0100

    More epochs

--- chatbot.py
+++ chatbot.py
@@ -174,7 +174,7 @@ model = tflearn.DNN(net, tensorboard_verbose=3)
 if os.path.exists("model.tflearn.index"):
     model.load("model.tflearn")
 else:
-    model.fit(training, output, n_epoch=500, batch_size=32, show_metric=True)
+    model.fit(training, output, n_epoch=750, batch_size=32, show_metric=True)
     model.save("model.tflearn")
 
 
